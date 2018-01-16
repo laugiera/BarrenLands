45987d1c862cdf7161b1aa1f6b22dd57e1cb3eaf
laugiera <laugier.anne.a@gmail.com>
2018-01-16 12:17:14 +0100

    sun and moon

--- tests/shaders/light.fs.glsl
+++ tests/shaders/light.fs.glsl
@@ -2,21 +2,25 @@
 
 // Entrées du shader
 // Interpolated values from the vertex shaders
-in vec2 UV;
-in vec3 VPosition;
-in vec3 Normal_cameraspace;
-in vec3 EyeDirection_cameraspace;
+in vec2 uV;
+in vec3 vPosition;
+in vec3 normal_cameraspace;
+in vec3 eyeDirection_cameraspace;
 
 // Ouput data
 out vec3 color;
 
 // Values that stay constant for the whole mesh.
-uniform mat4 MV;
 uniform sampler2D uTexture;
 uniform sampler2D uTexture2;
 uniform sampler2D uMoistureTexture;
 uniform int uSubDiv;
-uniform vec4 Light_cameraspace;
+uniform vec4 uLightDirSun;
+uniform float uLightIntensitySun;
+uniform vec3 uLightColorSun;
+uniform vec4 uLightDirMoon;
+uniform float uLightIntensityMoon;
+uniform vec3 uLightColorMoon;
 
 vec3 multiplyTexture(vec3 color, vec4 textureAlpha) {
     textureAlpha = textureAlpha * 0.3;
@@ -33,11 +37,11 @@ vec3 savane = vec3(255.f/255.f, 153.f/255.f, 0.f/255.f);
 vec3 craquele = vec3(255.f/255.f, 153.f/255.f, 102.f/255.f);
 vec3 sable = vec3(255.f/255.f, 255.f/255.f, 153.f/255.f);
 
-vec3 sableTexture = multiplyTexture(sable, texture(uTexture2, UV));
-vec3 toundraNeigeTexture = multiplyTexture(toundra_neige, texture(uTexture, UV));
+vec3 sableTexture = multiplyTexture(sable, texture(uTexture2, uV));
+vec3 toundraNeigeTexture = multiplyTexture(toundra_neige, texture(uTexture, uV));
 
-float height = VPosition.y;
-float moisture = (texture(uMoistureTexture, UV/uSubDiv)).x;
+float height = vPosition.y;
+float moisture = (texture(uMoistureTexture, uV/uSubDiv)).x;
 
 vec3 assignColor() {
 if (height < 0.25){
@@ -76,31 +80,25 @@ if (height < 0.25){
 }
 }
 
-void main() {
-   // Light emission properties
-   	// You probably want to put them as uniforms
-   	vec3 LightColor = vec3(0,0.2,1);
-   	float LightPower = 1.0f;
+vec3 getLightColor(vec3 lightColor, float lightPower, vec3 direction){
 
    	// Material properties
-   	vec3 MaterialDiffuseColor = assignColor();
-   	vec3 MaterialAmbientColor = vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
-   	vec3 MaterialSpecularColor = vec3(0.3,0.3,0.3);
-
+   	vec3 materialDiffuseColor = assignColor();
+   	vec3 materialAmbientColor = vec3(0.1,0.1,0.1) * materialDiffuseColor;
+   	vec3 materialSpecularColor = vec3(0.3,0.3,0.3);
 
    	// Normal of the computed fragment, in camera space
-   	vec3 n = normalize( Normal_cameraspace );
+   	vec3 n = normalize( normal_cameraspace );
    	// Direction of the light (from the fragment to the light)
-   	vec3 l = normalize( Light_cameraspace.xyz );
+   	vec3 l = normalize( direction.xyz );
    	// Cosine of the angle between the normal and the light direction,
    	// clamped above 0
    	//  - light is at the vertical of the triangle -> 1
    	//  - light is perpendicular to the triangle -> 0
    	//  - light is behind the triangle -> 0
    	float cosTheta = clamp( dot( n,l ), 0,1 );
-
    	// Eye vector (towards the camera)
-   	vec3 E = normalize(EyeDirection_cameraspace);
+   	vec3 E = normalize(eyeDirection_cameraspace);
    	// Direction in which the triangle reflects the light
    	vec3 R = reflect(-l,n);
    	// Cosine of the angle between the Eye vector and the Reflect vector,
@@ -109,13 +107,20 @@ void main() {
    	//  - Looking elsewhere -> < 1
    	float cosAlpha = clamp( dot( E,R ), 0,1 );
 
-  	color =
+  	vec3 color =
    		// Ambient : simulates indirect lighting
-   		MaterialAmbientColor +
+   		materialAmbientColor +
    		// Diffuse : "color" of the object
-   		MaterialDiffuseColor * LightColor * LightPower * cosTheta +
+   		materialDiffuseColor * lightColor * lightPower * cosTheta +
    		// Specular : reflective highlight, like a mirror
-   		MaterialSpecularColor * LightColor * LightPower * pow(cosAlpha,5);
+   		materialSpecularColor * lightColor * lightPower * pow(cosAlpha,5);
 
+   	return color;
+
+}
+
+void main() {
+    color = getLightColor(uLightColorMoon,uLightIntensityMoon,uLightDirMoon.xyz)
+    + getLightColor(uLightColorSun,uLightIntensitySun,uLightDirSun.xyz);
 }
 
