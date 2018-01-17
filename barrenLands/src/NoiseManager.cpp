3e7e8b2ad5cc2359d828c6a07d1904e4891776c5
ProvencalLeGaulois <etienne.juif@gmail.com>
2018-01-17 15:58:20 +0100

    Pour ne pas tout détruire

--- barrenLands/src/NoiseManager.cpp
+++ barrenLands/src/NoiseManager.cpp
@@ -37,8 +37,8 @@ float** NoiseManager::getElevationMap(const int width, const int height,const fl
             if(distance > height -5)
                 e = e -( b*pow(distance,c+0.5));
             elevationMap[x][y] = e;
-            std::cout << distance <<std::endl;
-            std::cout << e <<std::endl;
+            //std::cout << distance <<std::endl;
+            //std::cout << e <<std::endl;
         }
     }
     return elevationMap;
