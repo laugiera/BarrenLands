8839839eb71e48ee98cf858dc9d69d46973a2776
laugiera <laugier.anne.a@gmail.com>
2018-01-19 13:47:43 +0100

    fix shader

--- barrenLands/shaders/light.fs.glsl
+++ barrenLands/shaders/light.fs.glsl
@@ -27,18 +27,8 @@ uniform vec3 uColors[6];
 
 vec3 multiplyTexture(vec3 color, vec4 textureAlpha) {
     textureAlpha = textureAlpha * 0.3;
-    color.x = color.x + 0.2;
     return color - textureAlpha.xyz;
 }
-
-/*vec3 toundra_neige = vec3(200.f/255.f, 250.f/255.f, 245.f/255.f);
-vec3 toundra = vec3(168.f/255.f, 212.f/255.f, 206.f/255.f);
-vec3 roche = vec3(150.f/255.f, 150.f/255.f, 150.f/255.f);
-//vec3 toundra_herbe = vec3(153.f/255.f, 153.f/255.f, 51.f/255.f);
-vec3 herbe = vec3(153.f/255.f, 204.f/255.f, 0.f/255.f);
-vec3 savane = vec3(255.f/255.f, 153.f/255.f, 0.f/255.f);
-//vec3 craquele = vec3(255.f/255.f, 153.f/255.f, 102.f/255.f);
-vec3 sable = vec3(255.f/255.f, 255.f/255.f, 153.f/255.f);*/
 
 vec3 sableTexture = multiplyTexture(uColors[0], texture(uTexture1, uV));
 vec3 toundraNeigeTexture = multiplyTexture(uColors[3], texture(uTexture2, uV));
@@ -75,7 +65,7 @@ if (height < 0.25){
     } else if (moisture < 3.f/6.f){
         return uColors[2]; //toundra
     } else {
-        return uColors[3]; //snow
+        return toundraNeigeTexture; //snow
     }
 
 }
